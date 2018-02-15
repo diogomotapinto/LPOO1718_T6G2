9616440f7905d5796cc7a1b73b4d4b9c193d88fd
joaoagulha <up201607930@fe.up.pt>
2018-02-15 17:54:50 +0000

    findCharacter done

--- BasicGame/src/Map.java
+++ BasicGame/src/Map.java
@@ -3,98 +3,95 @@ public class Map {
 	private String[][] map;
 	private String[][] legend;
 	private Interface myInterface;
-	
-	class Legend{
+	private Hero hero;
+
+	class Legend {
 		public String wall;
 		public String door;
 		public String guard;
 		public String lever;
 		public String freeSpace;
 	}
-	
-	public Map()
-	{
-		this.map = new String[][] {{" X"," X"," X"," X"," X"," X"," X"," X"," X"," X"},
-				{" X"," H","  ","  "," I","  "," X","  "," G"," X"},
-				{" X"," X"," X","  "," X"," X"," X","  ","  "," X"},
-				{" X","  "," I","  "," I"," X"," X","  ","  "," X"},
-				{" X"," X"," X","  "," X"," X"," X","  ","  "," X"},
-				{" I","  ","  ","  ","  ","  ","  ","  ","  "," X"},
-				{" I","  ","  ","  ","  ","  ","  ","  ","  "," X"},
-				{" X"," X"," X","  "," X"," X"," X"," X","  "," X"},
-				{" X","  "," I","  "," I","  "," X"," K","  "," X"},
-				{" X"," X"," X"," X"," X"," X"," X"," X"," X"," X"}};
-				
-		this.legend = new String[][] {{"X - Wall"},
-			{"I - Door"},
-			{"H - Hero"},
-			{"G - Guard"},
-			{"k - lever"},
-			{"empty cell - free space"}};		
-				
-		this.myInterface = new Interface();	
-			
-		
+
+	public Map() {
+		this.map = new String[][] { { " X", " X", " X", " X", " X", " X", " X", " X", " X", " X" },
+				{ " X", " H", "  ", "  ", " I", "  ", " X", "  ", " G", " X" },
+				{ " X", " X", " X", "  ", " X", " X", " X", "  ", "  ", " X" },
+				{ " X", "  ", " I", "  ", " I", " X", " X", "  ", "  ", " X" },
+				{ " X", " X", " X", "  ", " X", " X", " X", "  ", "  ", " X" },
+				{ " I", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", " X" },
+				{ " I", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", " X" },
+				{ " X", " X", " X", "  ", " X", " X", " X", " X", "  ", " X" },
+				{ " X", "  ", " I", "  ", " I", "  ", " X", " K", "  ", " X" },
+				{ " X", " X", " X", " X", " X", " X", " X", " X", " X", " X" } };
+
+		this.legend = new String[][] { { "X - Wall" }, { "I - Door" }, { "H - Hero" }, { "G - Guard" }, { "k - lever" },
+				{ "empty cell - free space" } };
+		this.myInterface = new Interface();
+		int[] pos = this.findCharacter();
+		hero = new Hero(pos[0], pos[1]);
 	}
-	
-	public String[][] getMap()
-	{
+
+	public String[][] getMap() {
 		return this.map;
 	}
-	
-	public String[][] getLegend()
-	{
-		
+
+	public String[][] getLegend() {
+
 		return this.legend;
 	}
-	
-	
-	public void printMap()
-	{
-		for(int i = 0; i < this.map.length; i++)
-		{
-			for(int j = 0; j < this.map[i].length; j++)
-			{
+
+	public void printMap() {
+		for (int i = 0; i < this.map.length; i++) {
+			for (int j = 0; j < this.map[i].length; j++) {
 				System.out.print(this.map[i][j]);
 			}
 			System.out.println();
 		}
 	}
-	
-//	public void printArray(String[][] myArray)
-//	{
-//		for(int i = 0; i < myArray.length; i++)
-//		{
-//			for(int j = 0; j < myArray[i].length; j++)
-//			{
-//				System.out.print(myArray[i][j]);
-//			}
-//			System.out.println();
-//		}
-//	}
 
-	
+	// public void printArray(String[][] myArray)
+	// {
+	// for(int i = 0; i < myArray.length; i++)
+	// {
+	// for(int j = 0; j < myArray[i].length; j++)
+	// {
+	// System.out.print(myArray[i][j]);
+	// }
+	// System.out.println();
+	// }
+	// }
+
 	public void printLegend() {
-		
-		for(int i = 0; i < this.legend.length; i++){
-			
-			for(int j = 0; j < this.legend[i].length; j++){
-				
+
+		for (int i = 0; i < this.legend.length; i++) {
+
+			for (int j = 0; j < this.legend[i].length; j++) {
+
 				System.out.print(this.legend[i][j]);
 			}
-			
+
 			System.out.println();
 		}
 	}
-	
+
 	public boolean checkMove() {
-		
-		
+
 		return false;
 	}
-	
+
 	public int[] findCharacter() {
-		
+		int[] pos = { -1, -1 };
+		for (int i = 0; i < this.map.length; i++) {
+			for (int j = 0; j < this.map[0].length; j++) {
+				if (map[i][j].equals(" H")) {
+					pos[0] = i;
+					pos[1] = j;
+					return pos;
+				}
+			}
+		}
+		return pos;
 	}
-	
+
 }
