2c4cb595ee2a874b4fbb8b9c6ee0a564597c4b3f
joaoagulha <up201607930@fe.up.pt>
2018-02-16 12:33:27 +0000

    Methods implemented and Guard class partially implemented
    new global constants
    check end game conditions
    gaurd class implemented

--- BasicGame/src/Game.java
+++ BasicGame/src/Game.java
@@ -1,7 +1,11 @@
 
 public class Game {
-	static final String wall = "X";
-	static final String door = "I";
+	static final String WALL_CHAR = "X";
+	static final String DOOR_CHAR = "I";
+	static final String HERO_CHAR = "H";
+	static final String GUARD_CHAR = "G";
+	static final String LEVER_CHAR = "K";
+	static final String EMPTY_SPACE_CHAR = " ";
 
 	private String[][] map;
 	private String[][] legend;
@@ -9,27 +13,38 @@ public class Game {
 	private Character hero;
 
 	class Legend {
-		public String wall;
-		public String door;
+		public String WALL_CHAR;
+		public String DOOR_CHAR;
 		public String guard;
 		public String lever;
 		public String freeSpace;
 	}
 
 	public Game() {
-		this.map = new String[][] { { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-				{ "X", "H", " ", " ", "I", " ", "X", " ", "G", "X" },
-				{ "X", "X", "X", " ", "X", "X", "X", " ", " ", "X" },
-				{ "X", " ", "I", " ", "I", "X", "X", " ", " ", "X" },
-				{ "X", "X", "X", " ", "X", "X", "X", " ", " ", "X" },
-				{ "I", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-				{ "I", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-				{ "X", "X", "X", " ", "X", "X", "X", "X", " ", "X" },
-				{ "X", " ", "I", " ", "I", " ", "X", "K", " ", "X" },
-				{ "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" } };
+		this.map = new String[][] {
+				{ WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR,
+						WALL_CHAR },
+				{ WALL_CHAR, HERO_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, DOOR_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR,
+						EMPTY_SPACE_CHAR, GUARD_CHAR, WALL_CHAR },
+				{ WALL_CHAR, WALL_CHAR, WALL_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, EMPTY_SPACE_CHAR,
+						EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ WALL_CHAR, EMPTY_SPACE_CHAR, DOOR_CHAR, EMPTY_SPACE_CHAR, DOOR_CHAR, WALL_CHAR, WALL_CHAR,
+						EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ WALL_CHAR, WALL_CHAR, WALL_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, EMPTY_SPACE_CHAR,
+						EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ DOOR_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR,
+						EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ DOOR_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR,
+						EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ WALL_CHAR, WALL_CHAR, WALL_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR,
+						EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ WALL_CHAR, EMPTY_SPACE_CHAR, DOOR_CHAR, EMPTY_SPACE_CHAR, DOOR_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR,
+						LEVER_CHAR, EMPTY_SPACE_CHAR, WALL_CHAR },
+				{ WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR,
+						WALL_CHAR } };
 
-		this.legend = new String[][] { { "X - Wall" }, { "I - Door" }, { "H - Hero" }, { "G - Guard" }, { "k - lever" },
-				{ "empty cell - free space" } };
+		this.legend = new String[][] { { "X - WALL_CHAR" }, { "I - DOOR_CHAR" }, { "H - Hero" }, { "G - Guard" },
+				{ "k - lever" }, { "empty cell - free space" } };
 		this.myInterface = new Interface();
 		int[] pos = this.findCharacter();
 		hero = new Hero(pos[0], pos[1]);
@@ -40,7 +55,27 @@ public class Game {
 			printMap();
 			printLegend();
 			moveCharacter();
-		} while (true);
+		} while (check_game_conditions());
+	}
+
+	private boolean check_game_conditions() {
+		try {
+			if (map[this.hero.getXPosition() - 1][hero.getYPosition()] == GUARD_CHAR
+					|| map[this.hero.getXPosition() + 1][hero.getYPosition()] == GUARD_CHAR
+					|| map[this.hero.getXPosition()][hero.getYPosition() - 1] == GUARD_CHAR
+					|| map[this.hero.getXPosition()][hero.getYPosition() + 1] == GUARD_CHAR) {
+				System.out.print("\nPerdeu jogo");
+				printMap();
+				return false;
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+		}
+
+		if (this.hero.getXPosition() == 0 || this.hero.getYPosition() == 0) {
+			System.out.println("\nGanhou o jogo");
+			return false;
+		}
+		return true;
 	}
 
 	public String[][] getMap() {
@@ -48,107 +83,82 @@ public class Game {
 	}
 
 	public String[][] getLegend() {
-
 		return this.legend;
 	}
 
 	public void printMap() {
+		System.out.println("\n");
 		for (int i = 0; i < this.map.length; i++) {
 			for (int j = 0; j < this.map[i].length; j++) {
-				System.out.print(" " + this.map[i][j]);
+				System.out.print(EMPTY_SPACE_CHAR + this.map[i][j]);
 			}
 			System.out.println();
 		}
 	}
 
-	// public void printArray(String[][] myArray)
-	// {
-	// for(int i = 0; i < myArray.length; i++)
-	// {
-	// for(int j = 0; j < myArray[i].length; j++)
-	// {
-	// System.out.print(myArray[i][j]);
-	// }
-	// System.out.println();
-	// }
-	// }
-
 	public void printLegend() {
-
 		for (int i = 0; i < this.legend.length; i++) {
-
 			for (int j = 0; j < this.legend[i].length; j++) {
-
-				System.out.print(" " + this.legend[i][j]);
+				System.out.print(EMPTY_SPACE_CHAR + this.legend[i][j]);
 			}
-
 			System.out.println();
 		}
 	}
 
 	public void moveCharacter() {
 		String move = this.myInterface.getMove();
-		
-		try {
-		switch (move) {
-		case "u":
-			if (!(map[this.hero.getXPosition() - 1][hero.getYPosition()].equals(wall)
-					|| (map[this.hero.getXPosition() - 1][hero.getYPosition()].equals(door)))) {
-					
-				map[this.hero.getXPosition() ][hero.getYPosition()] = " ";
-				hero.setxPosition(hero.getXPosition() - 1);
-				checkLever();
-				map[this.hero.getXPosition() ][hero.getYPosition()] = "H";
-				
-			}
-			break;
-		case "d":
-			if (!(map[this.hero.getXPosition() + 1][hero.getYPosition()].equals(wall)
-					|| (map[this.hero.getXPosition() + 1][hero.getYPosition()].equals(door)))) {
-				
-				map[this.hero.getXPosition() ][hero.getYPosition()] = " ";
-				hero.setxPosition(hero.getXPosition() + 1);
-				checkLever();
-				map[this.hero.getXPosition() ][hero.getYPosition()] = "H";
-				
-			}
-			break;
-		case "l":
-			if (!(map[this.hero.getXPosition()][hero.getYPosition() - 1].equals(wall)
-					|| (map[this.hero.getXPosition()][hero.getYPosition() - 1].equals(door)))) {
-				
-				map[this.hero.getXPosition() ][hero.getYPosition()] = " ";
-				hero.setyPosition(hero.getYPosition() - 1);
-				checkLever();
-				map[this.hero.getXPosition() ][hero.getYPosition()] = "H";
-			}
-			break;
-		case "r":
 
-			if (!(map[this.hero.getXPosition()][hero.getYPosition() + 1].equals(wall)
-					|| (map[this.hero.getXPosition()][hero.getYPosition() + 1].equals(door)))) {
-				
-				map[this.hero.getXPosition() ][hero.getYPosition()] = " ";
-				hero.setyPosition(hero.getYPosition() + 1);
-				checkLever();
-				map[this.hero.getXPosition() ][hero.getYPosition()] = "H";
+		try {
+			switch (move) {
+			case "u":
+				if (!(map[this.hero.getXPosition() - 1][hero.getYPosition()].equals(WALL_CHAR)
+						|| (map[this.hero.getXPosition() - 1][hero.getYPosition()].equals(DOOR_CHAR)))) {
+					map[this.hero.getXPosition()][hero.getYPosition()] = EMPTY_SPACE_CHAR;
+					hero.setxPosition(hero.getXPosition() - 1);
+					checkLever();
+					map[this.hero.getXPosition()][hero.getYPosition()] = HERO_CHAR;
+				}
+				break;
+			case "d":
+				if (!(map[this.hero.getXPosition() + 1][hero.getYPosition()].equals(WALL_CHAR)
+						|| (map[this.hero.getXPosition() + 1][hero.getYPosition()].equals(DOOR_CHAR)))) {
+					map[this.hero.getXPosition()][hero.getYPosition()] = EMPTY_SPACE_CHAR;
+					hero.setxPosition(hero.getXPosition() + 1);
+					checkLever();
+					map[this.hero.getXPosition()][hero.getYPosition()] = HERO_CHAR;
+				}
+				break;
+			case "l":
+				if (!(map[this.hero.getXPosition()][hero.getYPosition() - 1].equals(WALL_CHAR)
+						|| (map[this.hero.getXPosition()][hero.getYPosition() - 1].equals(DOOR_CHAR)))) {
+					map[this.hero.getXPosition()][hero.getYPosition()] = EMPTY_SPACE_CHAR;
+					hero.setyPosition(hero.getYPosition() - 1);
+					checkLever();
+					map[this.hero.getXPosition()][hero.getYPosition()] = HERO_CHAR;
+				}
+				break;
+			case "r":
+				if (!(map[this.hero.getXPosition()][hero.getYPosition() + 1].equals(WALL_CHAR)
+						|| (map[this.hero.getXPosition()][hero.getYPosition() + 1].equals(DOOR_CHAR)))) {
+					map[this.hero.getXPosition()][hero.getYPosition()] = EMPTY_SPACE_CHAR;
+					hero.setyPosition(hero.getYPosition() + 1);
+					checkLever();
+					map[this.hero.getXPosition()][hero.getYPosition()] = HERO_CHAR;
+				}
+				break;
 			}
-			break;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// printMap();
+			System.err.println("IndexOutOfBoundsException " + e.getMessage());
 		}
-		}catch (IndexOutOfBoundsException e) {
-		    System.err.println("IndexOutOfBoundsException "+e.getMessage());
-		}
-		
-			
 
 	}
-	
-	
+
 	public int[] findCharacter() {
 		int[] pos = { -1, -1 };
 		for (int i = 0; i < this.map.length; i++) {
 			for (int j = 0; j < this.map[0].length; j++) {
-				if (map[i][j].equals("H")) {
+				if (map[i][j].equals(HERO_CHAR)) {
 					pos[0] = i;
 					pos[1] = j;
 					return pos;
@@ -157,18 +167,16 @@ public class Game {
 		}
 		return pos;
 	}
-	
+
 	public void checkLever() {
-		
-		if(map[this.hero.getXPosition()][hero.getYPosition()].equals("K")) {
+		if (map[this.hero.getXPosition()][hero.getYPosition()].equals(LEVER_CHAR)) {
 			for (int i = 0; i < this.map.length; i++) {
 				for (int j = 0; j < this.map[i].length; j++) {
-					if (map[i][j].equals("I")) {
+					if (map[i][j].equals(DOOR_CHAR)) {
 						map[i][j] = "S";
 					}
 				}
 			}
 		}
-				
 	}
 }
---
+++ BasicGame/src/Guard.java
@@ -0,0 +1,12 @@
+
+public class Guard extends Character {
+	private int course[][];
+
+	public Guard(int xPos, int yPos) {
+		super(xPos, yPos);
+		course = new int[][] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 },
+				{ 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 },
+				{ 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+	}
+
+}
