0df3610c7d07dc50cc39c9f301b5302a7bf23878
Diogo Mota Pinto <diogomotapinto@gmail.com>
2018-03-03 10:07:35 +0000

    dkeep.test

---
+++ BasicGame/src/dkeep/test/TestDungeonGameLogic.java
@@ -0,0 +1,14 @@
+package dkeep.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestDungeonGameLogic {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
